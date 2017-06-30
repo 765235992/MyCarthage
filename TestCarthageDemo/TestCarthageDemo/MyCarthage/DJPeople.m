605db92f36bf703ea24005c965a8f29ed0480005
765235992 <765235992@users.noreply.github.com>
2017-06-30 23:47:00 +0800

    2

---
+++ TestCarthageDemo/TestCarthageDemo/MyCarthage/DJPeople.m
@@ -0,0 +1,17 @@
+//
+//  DJPeople.m
+//  TestCarthageDemo
+//
+//  Created by Jone on 2017/6/30.
+//  Copyright © 2017年 河南东京Web俱乐部. All rights reserved.
+//
+
+#import "DJPeople.h"
+
+@implementation DJPeople
++ (void)eat
+{
+    NSLog(@"我爱吃苹果");
+}
+
+@end
---
+++ TestCarthageDemo/TestCarthageDemo/MyCarthage/MyCarthage.h
@@ -0,0 +1,13 @@
+//
+//  MyCarthage.h
+//  TestCarthageDemo
+//
+//  Created by Jone on 2017/6/30.
+//  Copyright © 2017年 河南东京Web俱乐部. All rights reserved.
+//
+
+#ifndef MyCarthage_h
+#define MyCarthage_h
+
+
+#endif /* MyCarthage_h */
